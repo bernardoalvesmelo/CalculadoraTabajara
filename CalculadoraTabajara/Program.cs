82db5367b191b686e0c3a1f4a62819c0f3ea11aa
Joao Marcelo <joaoamarceloalves585@gmail.com>
2023-06-07 14:21:50 -0300

    Menu concluido

--- CalculadoraTabajara/Program.cs
+++ CalculadoraTabajara/Program.cs
@@ -4,9 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Calculdora Tabajara");
+                Console.WriteLine("Digite 1 para Adição");
+                Console.WriteLine("Digite 2 para Subtrair");
+                Console.WriteLine("Digite 3 para Multiplicar");
+                Console.WriteLine("Digite 4 para Dividir");
+                Console.WriteLine("Digite S para sair");
+                
+                string operacao;
+                string operador;
+                string opcao = Console.ReadLine();
+                if (opcao == "S" || opcao == "s")
+                {
+                    break;
+                }
+                switch (opcao)
+                {
+                    case "1": operacao = "Adição"; operador = "+"; break;
+                    case "2": operacao = "Subtrair"; operador = "-"; break;
+                    case "3": operacao = "Multiplicar"; operador = "*"; break;
+                    case "4": operacao = "Dividir"; operador = "/"; break; 
+
+                }
+                
+            }
+          
         }
     }
 }
